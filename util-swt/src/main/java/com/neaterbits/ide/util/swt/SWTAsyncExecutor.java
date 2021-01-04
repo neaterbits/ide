a6ffb79bbf73a4874605e9d6d080a5760b9876e0
Nils Henrik Lorentzen <nils.lorentzen@gmail.com>
2021-01-04 12:09:02 +0100

    IDE-1 package update after BaseAsyncExecutor move

--- util-swt/src/main/java/com/neaterbits/ide/util/swt/SWTAsyncExecutor.java
+++ util-swt/src/main/java/com/neaterbits/ide/util/swt/SWTAsyncExecutor.java
@@ -2,7 +2,7 @@ package com.neaterbits.ide.util.swt;
 
 import org.eclipse.swt.widgets.Display;
 
-import com.neaterbits.ide.util.scheduling.BaseAsyncExecutor;
+import com.neaterbits.util.concurrency.scheduling.BaseAsyncExecutor;
 
 public final class SWTAsyncExecutor extends BaseAsyncExecutor<Void> {
 
